eb5d5d953cffb42b39d4e423fea9089acb7c90ee
rodriguezjoaquin <53922367+rodriguezjoaquin@users.noreply.github.com>
2021-10-20 22:03:41 -0300

    commit3

--- EjerciciosComplementariosLevel2/Ejercicio6.java
+++ EjerciciosComplementariosLevel2/Ejercicio6.java
@@ -6,9 +6,24 @@ import java.util.HashSet;
 public class Ejercicio6 {
     public static void main(String[] args) {
         HashSet<Empleado> empleado = new HashSet<>();
+        HashMap<Integer, Float> sueldo = new HashMap<>();
+        empleado.add(new Empleado("Diego Armando Maradona", 14276579, 8, 700));
+        empleado.add(new Empleado("Lionel Andres Messi", 33016244, 8, 800));
+        empleado.add(new Empleado("Wanda Nara", 35021765, 4, 600));
+        empleado.add(new Empleado("Mauro Icardi", 38098654, 3, 500));
+
+        for (Empleado e : empleado) {
+            System.out.println("Nombre: "+e.nombre+" - DNI: "+e.dni+" - Horas trabajadas: " +e.horasTrabajadas+" - Valor por hora: "+e.valorPorHora);
+        }
+
+        for (Empleado e : empleado) {
+            sueldo.put(e.getDni(), e.valor());
+            System.out.println("CLAVE (DNI): "+e.dni+" - SUELDO: "+e.valor());
+        }
+
     }
 
-    class Empleado {
+    public static class Empleado{
         String nombre;
         int dni;
         float horasTrabajadas;
